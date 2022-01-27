9b97aeb2a7f67237f2989b872af9b71f1be02f36
LegyenKristof <48032365+LegyenKristof@users.noreply.github.com>
2022-01-27 18:58:01 +0100

    Scannelés és kiírás kész

--- app/src/main/java/com/example/qrdolgozat/MainActivity.java
+++ app/src/main/java/com/example/qrdolgozat/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.qrdolgozat;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonScan, buttonKiir;
+    private TextView textViewOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+
+                intentIntegrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        textViewOutput.setText(intentResult.getContents());
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void init(){
+        buttonScan = findViewById(R.id.buttonScan);
+        buttonKiir = findViewById(R.id.buttonKiir);
+        textViewOutput = findViewById(R.id.textViewOutput);
     }
 }
